e7c92b4b8eb9334437053d0209478c216feaf115
andre <andrecvalera06@gmail.com>
2025-06-24 01:42:07 -0500

    no c

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "octree. cpp"
 using namespace std;
+
+
 int main() {
-    int a[9];
-    cout<<sizeof(*a)/sizeof(int);
+    Octree<int>::Point3D p(0,0,0);
+    Octree<int>::BoundingBox bb(p,1);
+    Octree<int>::Node n(bb);
+    Octree<int> a(n,100,100);
+
+
+
 }
